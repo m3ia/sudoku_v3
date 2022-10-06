ad2eef4bcfcc295ab4f16ac812c8123923e8df05
m3ia <meianatividad@gmail.com>
2022-10-06 15:58:20 -0700

    Create grid

--- client1/src/components/GameBoard.jsx
+++ client1/src/components/GameBoard.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from "react";
 const GameBoard = () => {
   const [problems, setProblems] = useState([]);
+  const [problem, setProblem] = useState("");
+  const [solution, setSolution] = useState("");
 
   useEffect(() => {
     // Get Problems
@@ -10,6 +12,8 @@ const GameBoard = () => {
         .then((res) => {
           setProblems([...res]);
           console.log("res", [...res]);
+          setProblem(res[0].problem);
+          setSolution(res[0].solution);
         });
     };
     getProblems();
@@ -17,10 +21,18 @@ const GameBoard = () => {
 
   return (
     <div className="container">
-      <h1>sudoku</h1>
-      {problems?.map((item, ind) => {
-        return <div key={ind}>{item.problem}</div>;
-      })}
+      <div>
+        <h1>sudoku</h1>
+      </div>
+      <div className="grid-container">
+        {problem.split("").map((item, ind) => {
+          return (
+            <div key={ind} className={`row${ind} grid-item`}>
+              {item}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
