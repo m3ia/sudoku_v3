3dbc6e1ef1c66dc954338aafe6a23bc1797bffd9
m3ia <meianatividad@gmail.com>
2022-10-06 12:54:30 -0700

    install pg-promise

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import pgPromise from 'pg-promise';
 
 const app = express();
 const PORT = 8080;
+const pgp = pgPromise({});
+const db = pgp('postgres://localhost:5432/sudoku');
 
 app.use(cors());
 app.use(bodyParser.json());
