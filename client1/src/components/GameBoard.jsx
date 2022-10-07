b602ab78f43ff75a8da4e2923b63b5edb168f032
m3ia <meianatividad@gmail.com>
2022-10-07 09:40:48 -0700

    Add and fix Dupe Checker

--- client1/src/components/GameBoard.jsx
+++ client1/src/components/GameBoard.jsx
@@ -4,19 +4,66 @@ const GameBoard = () => {
   const [problem, setProblem] = useState("");
   const [solution, setSolution] = useState("");
   const [currentInput, setCurrentInput] = useState(problem.slice());
+  const [dupeCells, setDupeCells] = useState([]);
 
   const updateUserInput = (val, ind) => {
     let inputArr = currentInput.split("");
     if (!val) {
       inputArr[ind] = "0";
+      if (dupeCells.includes(ind)) {
+        setDupeCells((prev) => prev.filter((item) => item !== ind));
+      }
     } else {
       inputArr[ind] = String(val);
     }
 
-    setCurrentInput(inputArr.join(""));
+    const newInputArr = inputArr.join("");
+    setCurrentInput(newInputArr);
     console.log(currentInput);
     console.log(solution);
+    setDupeCells([]);
+    checkDupes(newInputArr);
   };
+
+  // Checks for dupes each time an input is updated
+  function checkDupes(inputArr) {
+    // Map of all the items in the same row, mapped to an array of their indexes
+    let rowMaps = [...Array(9).keys()].map((i) => new Map());
+    let colMaps = [...Array(9).keys()].map((i) => new Map());
+    let boxMaps = [...Array(9).keys()].map((i) => new Map());
+    for (let i = 0; i < inputArr.length; i++) {
+      let row = Math.floor(i / 9);
+      let col = i % 9;
+      let boxGridRow = Math.floor(col / 3);
+      let boxGridCol = Math.floor(row / 3);
+      let box = boxGridRow * 3 + boxGridCol;
+
+      // Build out row DS
+      let rowMap = rowMaps[row];
+      let colMap = colMaps[col];
+      let boxMap = boxMaps[box];
+      [rowMap, colMap, boxMap].forEach((elem) => {
+        if (elem.has(inputArr[i])) {
+          elem.set(inputArr[i], [...elem.get(inputArr[i]), i]);
+        } else {
+          elem.set(inputArr[i], [i]);
+        }
+      });
+    }
+    // Check for dupes in every map and update background cell
+    [rowMaps, colMaps, boxMaps].forEach((dupeMaps) => {
+      for (let dupeMap of dupeMaps) {
+        for (let [key, val] of dupeMap) {
+          if (key === "0" || val.length === 1) {
+            continue;
+          }
+          for (let ind of val) {
+            setDupeCells((prev) => [...prev, ind]);
+          }
+        }
+      }
+    });
+  }
 
   // Check Answer
   const checkAnswer = () => {
@@ -45,7 +92,12 @@ const GameBoard = () => {
       <div className="grid-container">
         {problem.split("").map((item, ind) => {
           return (
-            <div key={ind} className={`row${ind} grid-item`}>
+            <div
+              key={ind}
+              className={`cell${ind} grid-item`}
+              style={{
+                backgroundColor: dupeCells.includes(ind) ? "yellow" : "white",
+              }}>
               {item !== "0" ? (
                 item
               ) : (
@@ -55,6 +107,11 @@ const GameBoard = () => {
                   id={`cell${ind}`}
                   onChange={(e) => updateUserInput(e.target.value, ind)}
                   maxLength="1"
+                  style={{
+                    backgroundColor: dupeCells.includes(ind)
+                      ? "yellow"
+                      : "white",
+                  }}
                 />
               )}
             </div>
